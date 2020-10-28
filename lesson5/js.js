b2ec7f435e070f3c1c737f466950d9b006fc4b7d
legendateam <undertaker176@gmail.com>
2020-10-28 22:44:36 +0200

    lesson5

---
+++ lesson5/js.js
@@ -0,0 +1,253 @@
+// завдання 1
+// let hi = "Hello" ;  
+// let kurs = "owu";
+// let domen = "ua";
+// let chuslo1 = 1;
+// let chuslo2 = 10;
+// let chuslo3 = -999;
+// let chuslo4 = 123;
+// let chuslopi = 3.14;
+// let chuslo5 = 2.7;
+// let age = 16;
+// let ispravda = true;
+// let isnepravda = false;
+
+// console.log(hi);
+// console.log(kurs);
+// console.log(domen);
+// console.log(chuslo1);
+// console.log(chuslo2);
+// console.log(chuslo3);
+// console.log(chuslo4);
+// console.log(chuslo5);
+// console.log(chuslopi);
+// console.log(age);
+// console.log(ispravda);
+// console.log(isnepravda);
+
+// alert(hi);
+// alert(kurs);
+// alert(domen);
+// alert(chuslo1);
+// alert(chuslo2);
+// alert(chuslo3);
+// alert(chuslo4);
+// alert(chuslo5);
+// alert(chuslopi);
+// alert(age);
+// alert(ispravda);
+// alert(isnepravda);
+
+// document.write(hi);
+// document.write(kurs);
+// document.write(domen);
+// document.write(chuslo1);
+// document.write(chuslo2);
+// document.write(chuslo3);
+// document.write(chuslo4);
+// document.write(chuslo5);
+// document.write(chuslopi);
+// document.write(age);
+// document.write(ispravda);
+// document.write(isnepravda);
+
+
+// завдання 2
+
+// let pruvit = "welcome";  
+// let name = "Yura";
+// let from = "ukraine";
+// let age1 = 23;
+// let bydunok = 23;
+// let kvartura = 66;
+// let nomervul = 548;
+// let rayon = 3;
+// let grn = 1.5576;
+// let head = 1;
+// let hasWork = true;
+// let hasWife = false;
+
+// console.log(pruvit);
+// console.log(name);
+// console.log(from );
+// console.log(age1);
+// console.log(bydunok);
+// console.log(kvartura);
+// console.log(nomervul);
+// console.log(rayon);
+// console.log(grn);
+// console.log(head);
+// console.log(hasWork);
+// console.log(hasWife);
+
+// alert(pruvit);
+// alert(name);
+// alert(from );
+// alert(age1);
+// alert(bydunok);
+// alert(kvartura);
+// alert(nomervul);
+// alert(rayon);
+// alert(grn);
+// alert(head);
+// alert(hasWork);
+// alert(hasWife);
+
+// document.write(pruvit);
+// document.write(name);
+// document.write(from);
+// document.write(age1);
+// document.write(bydunok);
+// document.write(kvartura);
+// document.write(nomervul);
+// document.write(rayon);
+// document.write(grn);
+// document.write(head);
+// document.write(hasWork);
+// document.write(hasWife);
+
+// Завдання 3
+
+// const PI = 3.141592653589793;
+// console.log(PI);
+// alert(PI);
+// document.write(PI);
+
+// const DV = -999;
+// console.log(DV);
+// alert(DV);
+// document.write(DV);
+
+// const IP = 128020201;
+// console.log(IP);
+// alert(IP);
+// document.write(IP);
+
+// const name = Yura;
+// console.log(name);
+// alert(name);
+// document.write(name);
+
+// const lastname = Cheban;
+// console.log(lastname);
+// alert(lastname);
+// document.write(lastname);
+
+// const FROM = LVIV;
+// console.log(FROM);
+// alert(FROM);
+// document.write(FROM);
+
+
+// завдання 4 
+
+// let firstname = prompt("What is you firstname?");
+// let lasttname = prompt("What is you lastname?");
+// let patronymicname = prompt("What is you patronymicname?");
+
+
+// alert(firstname);
+// console.log(firstname);
+// document.write(firstname);
+
+// alert(lasttname);
+// console.log(lasttname);
+// document.write(lasttname);
+
+// alert(patronymicname);
+// console.log(patronymicname);
+// document.write(patronymicname);
+
+// завдання 5
+
+// let FirstName = prompt("What is you firstname?");
+// let LastName = prompt("What is you LastName?");
+// let PatronymicName= prompt("What is you patronymicname?");
+// let person = {
+//     FirstName: FirstName,
+//     LastName: LastName,
+//     PatronymicName: PatronymicName
+// }
+
+// console.log(person);
+// завдання 6
+// let FirstName1 = prompt("What is you firstname?");
+// let LastName1 = prompt("What is you LastName?");
+// let Patronymicname1= prompt("What is you Patronymicname?");
+// let person1 = {
+//     FirstName: FirstName1,
+//     LastName: LastName1,
+//     Patronymicname: Patronymicname1
+// }
+
+// let FirstName2 = prompt("What is you firstname?");
+// let LastName2 = prompt("What is you LastName?");
+// let Patronymicname2= prompt("What is you Patronymicname?");
+// let person2 = {
+//     FirstName: FirstName2,
+//     LastName: LastName2,
+//     Patronymicname: Patronymicname2
+// }
+// console.log(person1);
+// console.log(person2);
+
+// завдання 7
+// let num1 = +prompt("Введіть перше число");
+// let num2 = +prompt("Введіть друге число");
+// let num3 = +prompt("Введіть трет'є число");
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+
+// завдання 8
+// let num1 = prompt("Введіть перше parseInt число") 
+// let num2 = prompt("Введіть друге parseIntt число") 
+// let num3 = prompt("Введіть трет'є parseInt число") 
+// let num4 = prompt("Введіть четверте parseInt число") 
+// let result = (parseInt(num1)) + (parseInt(num2)) + (parseInt(num3));
+// console.log (result);
+
+// завдання 9
+
+// let num1 = prompt("Введіть перше Float число")
+// let num2 = prompt("Введіть друге float число")
+// let num3 = prompt("Введіть трет'є Float число")
+// let result = (parseFloat(num1)) + (parseFloat(num2)) + (parseFloat(num3));
+// console.log (result);
+
+// завдання 10
+// let num1 = prompt("Введіть перше Math.round число")
+// let num2 = prompt("Введіть друге Math.round число")
+// let num3 = prompt("Введіть трет'є Math.round число")
+// let result = (Math.round(num1)) + (Math.round(num2)) + (Math.round(num3));
+// console.log (result);
+
+// завдання 11
+// let num1 = +prompt("Ввеліть Math.pow число");
+// let num2 = +prompt("Ввеліть Math.pow число");
+// console.log (Math.pow(num1, num2));
+
+// завдання 12
+// let a = typeof 100; 
+// let b = typeof '100'; 
+// let c = typeof true; 
+// let d = typeof undefined;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// // завдання13
+// console.log(5 < 6) // //  -> true
+// console.log(5 > 6) // -> false
+// console.log(5 == 6) // -> false
+// console.log(5 >= 6) // -> false
+// console.log(10 == 10) // -> true
+// console.log(10 <= 10)  // -> true
+// console.log(10 > 10) // -> false
+// console.log(10 < 10) // -> false
+// console.log(10 != 10) // -> false
+// console.log(123 === '123') //  -> false
+// console.log(123 == '123') //  -> true
